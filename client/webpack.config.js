47ba4745591d5e81142b336fde7e8cabb2aef8da
Lucas Young <48228936+uwlryoung@users.noreply.github.com>
2023-09-27 11:41:03 -0400

    Fixed a spelling mistake

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -43,7 +43,7 @@ module.exports = () => {
           {
             src: path.resolve('src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('asses', 'icons'),
+            destination: path.join('assets', 'icons'),
           },
         ],
       }),
