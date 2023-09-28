c2613bb5ce395813fae00265bc989efa1fe2621f
Lucas Young <48228936+uwlryoung@users.noreply.github.com>
2023-09-28 12:02:46 -0400

    Cleaned up comments in code. Removed todo.md

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Accepts some content and adds it to the database
 export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
@@ -20,15 +20,9 @@ export const putDb = async (content) => {
   const request = store.put({ jate: content});
   const result = await request;
   console.log("Data saved to the database", result)
-  // try {
-  //   const result = await request;
-  //   console.log("Data saved to the database", result)
-  // } catch (err) {
-  //   console.error('putDb not implemented', err);
-  // }
 }
 
-// TODO: Add logic for a method that gets all the content from the database
+// Gets all the content from the database
 export const getDb = async () => {
   console.log('GET from the database');
 
@@ -46,14 +40,6 @@ export const getDb = async () => {
 
   const result = await request;
   console.log('result.value', result);
-
-  // try {
-  //   const result = await request;
-  //   console.log('result.value', result);
-  //   return result;
-  // } catch (err) {
-  //   console.error('getDb not implemented', err);
-  // }
 }
 
 initdb();
